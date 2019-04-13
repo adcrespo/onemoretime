e29ba519688b881f3059953b23f999b81a74747d
Osqui Cubas <of.cubas@gmail.com>
2019-04-13 14:09:48 -0300

    Se comenzó a generar función para crear thread consola.

--- KERNEL/src/KERNEL.c
+++ KERNEL/src/KERNEL.c
@@ -10,8 +10,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/log.h>
 
 int main(void) {
+
+	/* 1. Creación de logger */
+
+	/* 2. Carga de configuración desde archivo */
+
+	/* 3. Inicializar variables */
+
+	/* 4. Creación de hilo para consola */
+	crear_hilo_consola();
+
 	puts("Dale que vaaaa"); /* prints Dale que vaaaa */
 	return EXIT_SUCCESS;
+}
+
+void crear_consola() {
+
+}
+
+void crear_hilo_consola() {
+	t_log* logger;
+	int thread_consola;
+	int hilo_consola = pthread_create(&thread_consola, NULL, crear_consola, NULL);
+	if (hilo_consola == -1) {
+		log_error(logger, "No se pudo generar el hilo para la consola.");
+	}
+	log_info(logger, "Se generó el hilo para la consola.");
 }
